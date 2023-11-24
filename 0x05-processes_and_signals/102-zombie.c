30fa2f122abfd0876f1dcd3a74d6f0268f283405
FairTee <stephenodjidja7@gmail.com>
2023-11-24 02:20:55 -0800

    Zombie killed

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+/**
+ * infinite_while - Creates an infinite loop
+ *
+ * Return: Always returns 0
+ */
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
+
+/**
+ * main - Creates 5 zombie processes
+ *
+ * Return: Always returns 0
+ */
+int main(void)
+{
+	pid_t zombie_pid;
+	int i;
+
+	for (i = 0; i < 5; i++)
+	{
+		zombie_pid = fork();
+
+		if (zombie_pid > 0)
+		{
+			/* Parent process */
+			printf("Zombie process created, PID: %d\n", zombie_pid);
+		}
+		else if (zombie_pid == 0)
+		{
+			/* Child process (zombie) */
+			exit(0);
+		}
+		else
+		{
+			perror("Fork error");
+			exit(1);
+		}
+	}
+
+	infinite_while();
+
+	return (0);
+}
